53245bda58b0f192550b725bbcf6123ce5ad22e1
Edouard Courty <edouard.courty@causeway.com>
2023-07-21 01:34:39 +0200

    Add Friends font & cleanup header

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -10,18 +10,26 @@ use Symfony\Component\Routing\Annotation\Route;
 #[Route(path: '/', name: 'main.')]
 class MainController extends AbstractController
 {
+    private const TEXT_COLOR_CLASS_NAMES = [
+        'text-danger',
+        'text-success',
+        'text-info',
+        'text-warning'
+    ];
+
     public function __construct(
         private readonly EpisodePicker $episodePicker
     ) {
     }
 
-    #[Route(path: 'random', name: 'random_episode')]
+    #[Route(path: '', name: 'random_episode')]
     public function getRandomEpisode(): Response
     {
         $episodeDto = $this->episodePicker->getRandomEpisode();
 
         return $this->render('episode.html.twig', [
-            'episode' => $episodeDto
+            'episode' => $episodeDto,
+            'randomTextColor' => self::TEXT_COLOR_CLASS_NAMES[array_rand(self::TEXT_COLOR_CLASS_NAMES)]
         ]);
     }
 }
